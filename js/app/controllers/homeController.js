8745ec2768d6a6c886556dee99cb9f649349323b
Sergio Esteban Restrepo Rhenals <sergiores24@gmail.com>
2019-04-29 16:33:18 -0500

    Creates Tasks group

--- js/app/controllers/homeController.js
+++ js/app/controllers/homeController.js
@@ -3,11 +3,35 @@ var controllerModule = angular.module('ToDoAppControllers');
 controllerModule.controller('homeController',
     function ($scope,tasksGroupService) {
     	$scope.tgroups=[]
+    	$scope.tasksGroup={};
+    	$scope.selectedGroup={};
+    	$scope.divGroup=false;
+    	$scope.divTask=false;
     	var getTasksGroups=function(){
     		tasksGroupService.getTasksGroups().then(function(response){
     			console.log(response.data);
     			$scope.tgroups=response.data;
     		});
-    	}
+    	};
     	getTasksGroups();
+    	$scope.createTasksGroup=function(){
+    		if($scope.tasksGroupForm.$valid){
+    			tasksGroupService.createTasksGroup($scope.tasksGroup).then((response)=>{
+    				$scope.tasksGroup={};
+    				$scope.divGroup=false;
+    				$scope.tasksGroupForm.$submitted=false;
+    				getTasksGroups();
+    			});
+    		}
+    	};
+    	$scope.toggledivGroup=function(){
+    		$scope.divGroup=!$scope.divGroup;
+    	}
+
+    	$scope.toggledivTask=function(group){
+    		$scope.divTask=!$scope.divTask;
+    		if($scope.divTask){
+    			$scope.selectedGroup=group;
+    		}else{$scope.selectedGroup={};}
+    	}
 });
--- js/app/services/tasksGroupService.js
+++ js/app/services/tasksGroupService.js
@@ -2,9 +2,13 @@ var servicesModule = angular.module('ToDoAppServices');
 
 servicesModule.factory('tasksGroupService', function ($http,api_url) {
     var baseUrl=api_url;
+    var headers={headers: {'Content-Type': 'application/x-www-form-urlencoded'}};
     return {
     	getTasksGroups: function(){
     		return $http.get(baseUrl+'/tasksgroup/getall');
+    	},
+    	createTasksGroup: function(tasksGroup){
+    		return $http.post(baseUrl+'/tasksgroup/create',tasksGroup,[headers]);
     	}
     };
 });
