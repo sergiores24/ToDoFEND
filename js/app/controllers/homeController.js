da08f7da4625c62353411c73f4525d74bc882a8a
Sergio Esteban Restrepo Rhenals <sergiores24@gmail.com>
2019-04-30 17:37:05 -0500

    Implementing Modals

--- js/app/controllers/homeController.js
+++ js/app/controllers/homeController.js
@@ -1,10 +1,9 @@
 var controllerModule = angular.module('ToDoAppControllers');
 
 controllerModule.controller('homeController',
-    function ($scope,tasksGroupService,taskService,userService) {
+    function ($scope,tasksGroupService,taskService,userService,$uibModal) {
     	$scope.tgroups=[];
       $scope.users=[];
-    	$scope.divGroup=false;
     	$scope.divTask=false;
       $scope.divUser=false;
       $scope.sentUser=false;
@@ -21,29 +20,6 @@ controllerModule.controller('homeController',
                 $scope.users=res.data;
         });
       }
-
-    	$scope.createTasksGroup=function(){
-    		if($scope.tasksGroupForm.$valid){
-    			tasksGroupService.createTasksGroup($scope.tasksGroup).then(function(response){
-    				$scope.tasksGroup={};
-    				$scope.divGroup=false;
-    				$scope.tasksGroupForm.$submitted=false;
-    				getTasksGroups();
-    			});
-    		}
-    	};
-
-    	$scope.createTask=function(){
-    		if($scope.taskForm.$valid && $scope.divTask){
-    			$scope.task.groupId=$scope.selectedGroup._id;
-    			taskService.createTask($scope.task).then(function(response){
-    				$scope.tasks={};
-    				$scope.divTask=false;
-    				$scope.taskForm.$submitted=false;
-    				getTasksGroups();
-    			})
-    		}
-    	};
 
         $scope.createUser=function(){
           $scope.sentUser=true;
@@ -66,26 +42,32 @@ controllerModule.controller('homeController',
             $scope.task.users.splice(index,1);
         };
 
-    	$scope.toggledivGroup=function(){
-    		$scope.divGroup=!$scope.divGroup;
-    		if(!$scope.divGroup){
-    			$scope.tasksGroup={};
-    			$scope.tasksGroupForm.$submitted=false;
-    		}
-    	};
+      $scope.tasksGroupModal = function(){
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'tasksGroup.html',
+            controller: 'tGroupController'
+          });
+        modalInstance.result.then(function(){
+          getTasksGroups();
+        });
+      }
 
-    	$scope.toggledivTask=function(group){
-    		$scope.divTask=!$scope.divTask;
-    		if($scope.divTask){
-    			$scope.selectedGroup=group;
-                getUsers();
-    		}else{
-                $scope.users=[];
-    			$scope.tasks={};
-    			$scope.selectedGroup={};
-    			$scope.taskForm.$submitted=false;
-    		}
-    	};
+      $scope.taskModal = function(group){
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'task.html',
+            controller: 'taskModalController',
+            resolve: {
+              group: function(){
+                return group;
+              }
+            }
+          });
+        modalInstance.result.then(function(){
+          getTasksGroups();
+        });
+      };
 
       $scope.toggledivUser=function(){
         $scope.divUser=!$scope.divUser;
@@ -94,4 +76,27 @@ controllerModule.controller('homeController',
           $scope.sentUser=false;
         }
       };
+});
+
+controllerModule.controller('tGroupController',function($scope,tasksGroupService,$uibModalInstance){
+  $scope.createTasksGroup=function(){
+    if($scope.tasksGroupForm.$valid){
+      tasksGroupService.createTasksGroup($scope.tasksGroup).then(function(response){
+        $uibModalInstance.close();
+      });
+    }
+  };
+});
+
+controllerModule.controller('taskModalController',function($scope,taskService,$uibModalInstance,group){
+  $scope.group=group;
+  console.log($scope.group);
+  $scope.createTask=function(){
+    if($scope.taskForm.$valid){
+      $scope.task.groupId=$scope.selectedGroup._id;
+      taskService.createTask($scope.task).then(function(response){
+        $uibModalInstance.close();
+      });
+    }
+  };
 });
